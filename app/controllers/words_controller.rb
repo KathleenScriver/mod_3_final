5ea464e16bd20ba4167e1c639016b0166a67e8d7
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-30 09:43:52 -0600

    Adds facade to controller

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,5 +1,7 @@
 class WordsController < ApplicationController
   def index
+    require "pry"; binding.pry
+    @word_search_results = WordSearchFacade.new(params[:word])
     redirect_to '/'
   end
 end
