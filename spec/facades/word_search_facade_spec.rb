9b12d5f5f9aef1d0af2b3935a33aa7e1d5de0fd4
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-30 09:52:20 -0600

    Adds test for getting the example sentences

--- spec/facades/word_search_facade_spec.rb
+++ spec/facades/word_search_facade_spec.rb
@@ -9,4 +9,16 @@ describe WordSearchFacade do
   it 'is created with word attribute' do
     expect(subject.word).to eq('mindfulness')
   end
+
+  context 'instance methods' do
+    context '#examples' do
+      it 'should return sentence examples of word in hash with region' do
+        text_1 = "Meditation is one way to express mindfulness in a dedicated, concentrated manner."
+        text_2 = "At such times, mindfulness of the practice of patience and the application of certain techniques will help us to continue generating this attitude."
+
+        expected_hash = { "British" => text_1, "Canadian" => text_2 }
+        expect(subject.examples).to eq(expected_hash)
+      end
+    end
+  end
 end
