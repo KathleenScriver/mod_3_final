afa2da0eaef84e941de40f9261e8476fb819095b
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-30 10:14:36 -0600

    Adds method to pull api data for region and text

--- app/facades/word_search_facade.rb
+++ app/facades/word_search_facade.rb
@@ -3,4 +3,23 @@ class WordSearchFacade
   def initialize(word)
     @word = word
   end
+
+  def examples
+    app_id = ENV['oxford_app_id']
+    app_key = ENV['oxford_app_key']
+    conn = Faraday.new('https://od-api.oxforddictionaries.com') do |faraday|
+      faraday.headers['app_id'] = app_id
+      faraday.headers['app_key'] = app_key
+      faraday.adapter Faraday.default_adapter
+    end
+    response = conn.get("/api/v1/entries/en/#{word}/sentences")
+    raw_info = JSON.parse(response.body, symbolize_names: true)[:results].first[:lexicalEntries].first[:sentences]
+    regional_info = raw_info.select do |info_hash|
+      info_hash[:regions].first == "Canadian" || info_hash[:regions].first == "British"
+    end
+    t = regional_info.map do |regional_hash|
+      Example.new(regional_hash[:regions], regional_hash[:text])
+    end
+    require "pry"; binding.pry
+  end
 end
---
+++ app/models/example.rb
@@ -0,0 +1,7 @@
+class Example
+  attr_reader :region, :text
+  def initialize(region, text)
+    @region = region
+    @text = text
+  end
+end 
