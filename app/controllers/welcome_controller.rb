b72888d5e1d4a8ca6140f371475a5be20e3b49ba
Kathleen Yruegas <kathleenecn@gmail.com>
2018-10-30 10:37:32 -0600

    Fixes form to have results show on welcome index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @word_search_results = WordSearchFacade.new(params[:word]) if params[:word]
   end
 end
--- app/controllers/words_controller.rb
+++
@@ -1,6 +0,0 @@
-class WordsController < ApplicationController
-  def index
-    @word_search_results = WordSearchFacade.new(params[:word])
-    redirect_to '/'
-  end
-end
